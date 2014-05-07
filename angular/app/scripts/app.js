6570f41acf740b765e70f78652534cffe8512715
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2014-05-07 11:05:53 -0700

    Create Angular Site

--- angular/app/scripts/app.js
+++ angular/app/scripts/app.js
@@ -19,7 +19,7 @@ angular.module('communityCapitalApp', [
     })
     .state('page.signup', {
       url: "/signup",
-      templateUrl: "views/investor.signup.html",
+      templateUrl: "views/signup.html",
     })
     .state('page.investor', {
       url: "/investor",
@@ -27,7 +27,45 @@ angular.module('communityCapitalApp', [
     })
     .state('page.investor.search', {
       url: "/search",
-      templateUrl: "views/investor.search.html"
+      templateUrl: "views/investor.search.html",
+      controller: function($scope) {
+
+        $scope.results1 = [
+          {
+            name: 'Property 1', 
+            image: 'images/building1.jpg', 
+            offering: '$5,000,000', 
+            term: '3 years',
+            minimum: '$10,000',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar mattis dui sit amet tristique. Suspendisse potenti. Aliquam dictum nunc non ante ultrices, vitae luctus tortor consectetur.'
+          },
+          {
+            name: 'Property 2',
+            image: 'images/building2.jpg',
+            offering: '$2,000,000',
+            term: '2 years 6 months',
+            minimum: '$3,000',
+            description: 'Pellentesque vitae quam et urna egestas vehicula et ut quam. Nullam rutrum eleifend ante sed egestas. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+          }];
+        $scope.results2 = [ 
+          {
+            name: 'Property 3',
+            image: 'images/appartment.jpg',
+            offering: '$3,000,000',
+            term: '3 years',
+            minimum: '$4,000',
+            description: 'Cras tristique ligula eget eros dapibus semper. Mauris a mollis nulla, a luctus ligula. Nam in fringilla libero. Aenean dapibus gravida dui sed sodales. Proin vestibulum nulla vel libero pulvinar aliquet.'
+          },
+          { 
+            name: 'Property 4',
+            image: 'images/building3.jpg',
+            offering: '$3,500,000',
+            term: '2 years',
+            minimum: '$6,000',
+            description: ''
+          }
+        ];
+      }
     })
     .state('page.investor.investments', {
       url: "/investments",
